f7cbcbb0cadd8e478b3482d573126a4e820472a1
Prajesh S <praju.chess2000@gmail.com>
2026-08-04 10:57:20 +0530

    added new file multiply and updated docstring for add

--- add.py
+++ add.py
@@ -1,3 +1,12 @@
-# Sum function
 def sum_numbers(a, b):
+    """
+    Sums two numbers.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The sum of a and b.
+    """
     return a + b
---
+++ multiply.py
@@ -0,0 +1,12 @@
+def multiply(x, y):
+    """
+    Multiplies two numbers.
+
+    Parameters:
+    x (int or float): The first number.
+    y (int or float): The second number.
+
+    Returns:
+    int or float: The product of x and y.
+    """
+    return x * y
