8fea154ed3503c05d5d2eff2947d1b64887b2dbf
Prajesh S <praju.chess2000@gmail.com>
2026-08-05 10:22:28 +0530

    added doc string in subract.py

--- subract.py
+++ subract.py
@@ -1,3 +1,12 @@
 # Subtraction function
 def subtract_numbers(a, b):
+    """
+    Subtracts the second number from the first number.
+
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+    Returns:
+    int: The result of a minus b.
+    """
     return a - b
